7f825a752ffa4810bb877ad0fb99200a2af1dc86
NamTaeHee1 <skaxogml777@naver.com>
2020-12-07 10:43:53 +0900

    [ADD] Languages
    [ADD] - LanguagesUpload

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject Target;
+
+    void Update()
+    {
+        transform.position = new Vector3(Target.gameObject.transform.position.x, 
+            Target.gameObject.transform.position.y + 6, Target.gameObject.transform.position.z - 15);
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Transform PlayerTransform;
+    Vector3 moveVec;
+    Animator anim;
+
+    void Start()
+    {
+        PlayerTransform = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        PlayerAnimation();
+    }
+    void FixedUpdate()
+    {
+        PlayerMoveAndRotate();
+    }
+
+    void PlayerMoveAndRotate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        bool shiftDown = Input.GetKey(KeyCode.LeftShift);
+
+        moveVec = new Vector3(h, 0, v).normalized;
+
+        PlayerTransform.position += moveVec * (shiftDown ? 20.0f : 10.0f) * Time.deltaTime;
+        PlayerTransform.LookAt(PlayerTransform.position + moveVec);
+    }
+
+    void PlayerAnimation()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            anim.SetBool("isWalk", true);
+            if (Input.GetKey(KeyCode.LeftShift))
+                anim.SetBool("isRun", true);
+            else
+                anim.SetBool("isRun", false);
+        }
+        else
+            anim.SetBool("isWalk", false);
+    }
+}
